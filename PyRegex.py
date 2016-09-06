33d4df23f553d0e6e2b41d54582921829eb57e70
yuanjinlin <linyuanjin@yscredit.com>
2016-09-06 17:13:02 +0800

    添加了一些比较简单的实现

--- PyRegex.py
+++ PyRegex.py
@@ -1,3 +1,7 @@
+match_pool = []
+
+
+# FA for 'abc'
 def gen_abc(entry: int):
     out = entry
     in_char = yield
@@ -19,12 +23,53 @@ def gen_abc(entry: int):
         yield 'fail'
 
 
+# desire pattern for 'abc'
+def gen_abc_1(entry: int):
+    out = entry
+    for expected in ('a', 'b', 'c'):
+        in_char = yield
+        if in_char == expected:
+            out += 1
+        else:
+            yield 'fail'
+    print('accept', entry, out)
+    yield 'accept'
+
+
+def make_gen(s: str):
+    token_id = 'LYJ'
+
+    def generator(entry: int):
+        out = entry
+        for expected in s:
+            in_char = yield
+            if in_char == expected:
+                out += 1
+            else:
+                yield 'fail'
+        print('accept', entry, out)
+        print('for', token_id)
+        yield 'accept'
+
+    return generator
+
+
+# FA for 'a|b|c'
+def gen_a_b_c(entry: int):
+    for char in ('a', 'b', 'c'):
+        gen = make_gen(char)(entry)
+        next(gen)
+        match_pool.append(gen)
+    yield
+    yield 'fail'
+
+
 if __name__ == '__main__':
-    string = 'ababcababc'
-    match_pool = []
+    string = 'abexabcsagvbafbec'
+    _gen_abc = make_gen('abc')
     for i, char in enumerate(string):
         new_pool = []
-        gen = gen_abc(i)
+        gen = gen_a_b_c(i)
         next(gen)
         match_pool.append(gen)
         for nfa in match_pool:
