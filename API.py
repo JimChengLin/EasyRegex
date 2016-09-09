d1df5440a4e8df9a8b883de70197ff5945e9ec50
yuanjinlin <linyuanjin@yscredit.com>
2016-09-09 17:35:23 +0800

    基础API

--- API.py
+++ API.py
@@ -1,9 +1,16 @@
 class M:
-    def __init__(self, target: str):
-        self.target = target
-        self.cursor = self
+    def __init__(self, target_rule):
+        self.target_rule = target_rule
         self.child = None
         self.sibling_l = []
+
+    @property
+    def is_matcher(self):
+        return isinstance(self.target_rule, str)
+
+    @property
+    def is_wrapper(self):
+        return isinstance(self.target_rule, M)
 
     def __str__(self):
         if self.sibling_l:
@@ -22,6 +29,12 @@ class M:
         assert isinstance(other, M)
         self.sibling_l.append(other)
         return self
+
+    def __sub__(self, other):
+        pass
+
+    def n(self, n):  # number
+        pass
 
 
 if __name__ == '__main__':
