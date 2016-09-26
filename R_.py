ed3b0cb3a00ff07cf0a08d78f92c0b1d263e2eeb
yuanjinlin <linyuanjin@yscredit.com>
2016-09-26 16:36:36 +0800

    decorate

--- R_.py
+++ R_.py
@@ -22,22 +22,59 @@ class Result:
             return self.epoch == other.epoch and self.ed == other.ed
 
 
+class Success(Result):
+    pass
+
+
 class Fail(Result):
     pass
 
 
 def make_gen(target, num: tuple) -> callable:
-    # make -> gen -> fa
+    # gen -> fa
     if isinstance(target, str):
-        def gen(epoch: int, op: int, table: dict):
-            log = table.get('$log', False)
+        def gen(epoch: int, op: int, table: dict, log: bool) -> iter:
+            table = table.copy()
             ed = op
-            prev_str = ''
+            table['$prev_str'] = ''
             for expect_char in target:
                 recv_char = yield 'GO'
                 if log:
-                    prev_str += recv_char
+                    table['$prev_str'] += recv_char
                 if recv_char == expect_char:
                     ed += 1
                 else:
-                    yield Fail(epoch, ed, )
+                    yield Fail(epoch, ed, table)
+            yield Success(epoch, ed, table)
+            yield 'NO'
+    elif callable(target):
+        def gen(epoch: int, op: int, table: dict, log: bool) -> iter:
+            table = table.copy()
+            recv_char = yield 'GO'
+            table['$prev_str'] = recv_char if log else ''
+            if target(recv_char, (epoch, op, table)):
+                yield Success(epoch, op + 1, table)
+            else:
+                yield Fail(epoch, op + 1, table)
+            yield 'NO'
+    else:
+        raise Exception
+
+    if num == (1, 1):
+        return gen
+    else:
+        def decorate_g(epoch: int, op: int, table: dict, log: bool) -> iter:
+            counter = 0
+            from_num, to_num = num
+            if isinstance(from_num, str):
+                from_num = to_num = len(table.get(from_num, ()))
+
+            inner_gen = gen(epoch, op, table, log)
+            next(inner_gen)
+            curr_state = Result(epoch, op, table) if from_num == 0 else 'GO'
+            while counter < to_num:
+                recv_char = yield curr_state
+                echo = inner_gen.send(recv_char)
+
+                if isinstance():
+                    pass
