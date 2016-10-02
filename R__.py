595112e63c29a586269f57ca9227c3c30f0d044d
Jim <jimzuolin@gmail.com>
2016-10-02 12:51:30 +0800

    还需核心代码

--- R__.py
+++ R__.py
@@ -27,7 +27,7 @@ class Res:
             return self.epoch == other.epoch and self.ed == other.ed
 
     def copy(self):
-        return Res(self.epoch, self.ed, self.nth, self.prev_str, self.capture_t)
+        return Res(**self.__dict__)
 
     def as_success(self):
         self.__class__ = Success
@@ -40,17 +40,21 @@ class Res:
 
 class Success(Res):
     def invert(self):
-        self.__class__ = Fail
-        return self
+        return self.as_fail()
+
+    def __bool__(self):
+        return True
 
 
 class Fail(Res):
     def invert(self):
-        self.__class__ = Success
-        return self
+        return self.as_success()
+
+    def __bool__(self):
+        return False
 
 
-def make_gen(target, num_t: tuple):  # fa
+def make_gen(target, num_t: tuple):  # -> fa
     if isinstance(target, Iterable):
         def gen(prev_res: Res, log: bool):
             res = prev_res.copy()
@@ -62,9 +66,9 @@ def make_gen(target, num_t: tuple):  # fa
                     res.ed += 1
                 else:
                     yield res.as_fail()
-                    yield 'NO'
+                    yield 'STOP'
             yield res.as_success()
-            yield 'NO'
+            yield 'STOP'
     elif isinstance(target, Callable):
         def gen(prev_res: Res, log: bool):
             res = prev_res.copy()
@@ -74,7 +78,7 @@ def make_gen(target, num_t: tuple):  # fa
                 yield res.as_success()
             else:
                 yield res.as_fail()
-            yield 'No'
+            yield 'STOP'
     else:
         raise Exception
 
@@ -84,7 +88,7 @@ def make_gen(target, num_t: tuple):  # fa
         def decorate_gen(prev_res: Res, log: bool):
             counter = 0
             from_num, to_num = explain_n(prev_res, num_t)
-            curr_state = 'OK' if from_num == 0 else 'GO'
+            curr_state = 'OPTION' if from_num == 0 else 'GO'
             if to_num == 0:
                 yield curr_state
 
@@ -104,15 +108,11 @@ def make_gen(target, num_t: tuple):  # fa
                         yield echo
                 elif isinstance(echo, Fail):
                     yield echo
-                    yield 'NO'
+                    break
                 curr_state = echo
-            yield 'NO'
+            yield 'STOP'
 
         return decorate_gen
-
-
-def is_l(obj):
-    return isinstance(obj, list)
 
 
 def parse_n(num):
@@ -153,12 +153,12 @@ def str_n(num_t: tuple):
 
 
 def explain_n(res: Res, num_t: tuple):
-    epoch, op, capture_d = res.epoch, res.ed, res.capture_d
+    epoch, ed, capture_d = res.epoch, res.ed, res.capture_d
     from_num, to_num = num_t
     if isinstance(from_num, str):
         from_num = to_num = len(capture_d.get(from_num, ()))
     elif isinstance(from_num, Callable):
-        from_num, to_num = from_num(epoch, op, capture_d), to_num(epoch, op, capture_d)
+        from_num, to_num = from_num(epoch, ed, capture_d), to_num(epoch, ed, capture_d)
     assert 0 <= from_num <= to_num
     return from_num, to_num
 
@@ -166,19 +166,19 @@ def explain_n(res: Res, num_t: tuple):
 class R:
     def __init__(self, target_rule, num=None, name: str = None):
         self.target_rule = target_rule
-        self.num = parse_n(num)
+        self.num_t = parse_n(num)
         self.name = name
 
+        self.and_r = None
+        self.or_r_l = []
         self.next_r = None
-        self.sibling_l = []
-        self.demand_r = None
 
         if self.is_matcher:
             self.fa_l = []
-            self.gen = make_gen(self.target_rule, self.num)
+            self.gen = make_gen(self.target_rule, self.num_t)
 
         self.xor_r = None
-        self.invert = Fail
+        self.invert = False
 
     @property
     def is_matcher(self):
@@ -188,36 +188,36 @@ class R:
     def is_wrapper(self):
         return isinstance(self.target_rule, R)
 
-    def __and__(self, other: 'R') -> 'R':
+    def __and__(self, other: 'R'):
         other = other.clone()
         self_clone = self.clone()
-        if self_clone.sibling_l:
+        if self_clone.or_r_l:
             cursor = R(self_clone)
         else:
             cursor = self_clone
-            while cursor.demand_r is not None:
-                cursor = cursor.demand_r
-        cursor.demand_r = other
+            while cursor.and_r is not None:
+                cursor = cursor.and_r
+        cursor.and_r = other
         return self_clone
 
-    def __or__(self, other: 'R') -> 'R':
+    def __or__(self, other: 'R'):
         other = other.clone()
         self_clone = self.clone()
-        self_clone.sibling_l.append(other)
+        self_clone.or_r_l.append(other)
         return self_clone
 
-    def __xor__(self, other: 'R') -> 'R':
+    def __xor__(self, other: 'R'):
         other = other.clone()
         self_clone = R(self.clone())
         self_clone.xor_r = other
         return R(self_clone)
 
-    def __invert__(self) -> 'R':
+    def __invert__(self):
         self_clone = R(self.clone())
         self_clone.invert = True
         return R(self_clone)
 
-    def __call__(self, *other_l) -> 'R':
+    def __call__(self, *other_l):
         if not other_l:
             return self
         self_clone = self.clone()
@@ -230,12 +230,15 @@ class R:
         return R(self_clone)
 
     def __repr__(self):
-        s = str(self.target_rule)
+        if isinstance(self.target_rule, Callable):
+            s = '<{}>'.format(self.target_rule)
+        else:
+            s = str(self.target_rule)
 
-        def group() -> str:
+        def group():
             return '[{}]'.format(s)
 
-        def is_group() -> bool:
+        def is_group():
             return s.startswith('[') and s.endswith(']')
 
         if self.xor_r:
@@ -243,15 +246,15 @@ class R:
         elif self.invert:
             s = '~[{}]'.format(s)
         else:
-            if self.demand_r:
-                s += '&{}'.format(self.demand_r)
-            if self.sibling_l:
-                s += '|' + '|'.join(str(i) for i in self.sibling_l)
+            if self.and_r:
+                s += '&' + str(self.and_r)
+            if self.or_r_l:
+                s += '|' + '|'.join(str(i) for i in self.or_r_l)
                 s = group()
-            if (self.demand_r or (self.is_wrapper and self.target_rule.demand_r)) and self.next_r and not is_group():
+            if (self.and_r or (self.is_wrapper and self.target_rule.and_r)) and self.next_r and not is_group():
                 s = group()
 
-        num_str = str_n(self.num)
+        num_str = str_n(self.num_t)
         if num_str:
             if len(s) > 1 and not is_group():
                 s = group()
@@ -261,10 +264,10 @@ class R:
         return s
 
     def broadcast(self, char):
-        return char
+        return self, char
 
     def active(self, prev_res: Res, log=False, append=True):
-        log = bool(log or self.demand_r or self.xor_r)
+        log = bool(log or self.and_r or self.xor_r)
         if self.is_matcher:
             fa = self.gen(prev_res, log)
             echo = next(fa)
@@ -273,47 +276,39 @@ class R:
         else:
             echo = self.target_rule.active(prev_res, log)
             if echo == 'GO':
-                from_num, _ = explain_n(prev_res, self.num)
+                from_num, _ = explain_n(prev_res, self.num_t)
                 if from_num == 0:
-                    echo = 'OK'
+                    echo = 'OPTION'
 
         if self.xor_r:
             xor_echo = self.xor_r.active(prev_res, append=False)
-            if xor_echo == echo:
-                echo = 'GO'
-            else:
-                echo = 'OK'
+            echo = 'GO' if xor_echo == echo else 'OPTION'
         elif self.invert:
-            if echo == 'GO':
-                echo = 'OK'
-            else:
-                echo = 'GO'
+            echo = 'GO' if echo == 'OPTION' else 'OPTION'
         else:
-            if echo == 'OK' and self.demand_r:
-                demand_echo = self.demand_r.active(prev_res, append=False)
-                if demand_echo != 'OK':
+            if echo == 'OPTION' and self.and_r:
+                and_echo = self.and_r.active(prev_res, append=False)
+                if and_echo != 'OPTION':
                     echo = 'GO'
-            for sibling in self.sibling_l:
-                sibling_echo = sibling.active(prev_res, append=False)
-                if sibling_echo == 'OK':
-                    echo = 'OK'
+            for or_r in self.or_r_l:
+                or_r_echo = or_r.active(prev_res, append=False)
+                if or_r_echo == 'OPTION':
+                    echo = 'OPTION'
                     break
         return echo
 
-    def match(self, source: Iterable) -> list:
+    def match(self, source: Iterable):
         res_l = []
-        for i, char in chain([(-1, MatchStart)], enumerate(chain(source, [MatchEnd]))):
+        for i, char in chain([(-1, MatchOP)], chain(enumerate(chain(source, [MatchED])))):
             self.active(Res(i, i))
-            this_echo = self.broadcast(char)
-            if is_l(this_echo):
-                res_l.extend(this_echo)
+            res_l.extend(filter(bool, self.broadcast(char)))
         return res_l
 
-    def clone(self) -> 'R':
-        matcher = R(self.target_rule if self.is_matcher else self.target_rule.clone(), self.num, self.name)
-        if self.demand_r:
-            matcher.demand_r = self.demand_r.clone()
-        matcher.sibling_l.extend(i.clone() for i in self.sibling_l)
+    def clone(self):
+        matcher = R(self.target_rule if self.is_matcher else self.target_rule.clone(), self.num_t, self.name)
+        if self.and_r:
+            matcher.and_r = self.and_r.clone()
+        matcher.or_r_l.extend(i.clone() for i in self.or_r_l)
         if self.xor_r:
             matcher.xor_r = self.xor_r.clone()
         matcher.invert = self.invert
@@ -321,9 +316,9 @@ class R:
             matcher.next_r = self.next_r.clone()
 
 
-class MatchStart:
+class MatchOP:
     pass
 
 
-class MatchEnd:
+class MatchED:
     pass
