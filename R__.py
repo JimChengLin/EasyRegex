81c217061e90170025efd8e26d45e001bcf58a4c
yuanjinlin <linyuanjin@yscredit.com>
2016-10-07 17:32:50 +0800

    @ bug fix

--- R__.py
+++ R__.py
@@ -308,7 +308,8 @@ class R:
                     from_num, to_num = explain_n(res, self.num_t)
                     res.nth += 1
                     if res.nth < to_num:
-                        self.active(res)
+                        self.active(res.clone(capture_t=(*res.capture_t,
+                                                         (self.name, res.op, res.ed)) if self.name else res.capture_t))
                     if from_num <= res.nth <= to_num:
                         res.nth = 0  # 已激活
                         res_l.append(res)
