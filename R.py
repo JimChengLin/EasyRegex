0c7ca22402a30df315bc5148af758be6645a0426
Jim <jimzuolin@gmail.com>
2016-10-09 18:41:39 +0800

    更名

--- R.py
+++ R.py
@@ -324,14 +324,14 @@ class R:
             s += str(self.next_r)
         return s
 
-    def broadcast(self, char=None, prev_str=None, i=None):
+    def broadcast(self, char=None, prev_l: list = None, i: int = None):
         if char is None:
             if self.is_matcher:
                 self.fa_l.clear()
             if self.mode is not Mode.All:
                 self.best_length = None
         if self.next_r:
-            next_res_l = self.next_r.broadcast(char, prev_str, i)
+            next_res_l = self.next_r.broadcast(char, prev_l, i)
 
         if self.is_matcher:
             self_res_l = []
@@ -345,7 +345,7 @@ class R:
                         self_res_l.append(echo)
                 self.fa_l = fa_l
         else:
-            self_res_l = self.target_rule.broadcast(char, prev_str, i)
+            self_res_l = self.target_rule.broadcast(char, prev_l, i)
             res_l = []
             for res in self_res_l:
                 if not res:
@@ -369,8 +369,8 @@ class R:
                     if isinstance(k, int) and k == id(self):
                         op = item.pop()
                         break
-                for char in prev_str[op + 1:res.ed + 1]:
-                    xor_res_l = self.xor_r.broadcast(char, prev_str)
+                for char in prev_l[op + 1:res.ed + 1]:
+                    xor_res_l = self.xor_r.broadcast(char, prev_l)
                 self.xor_r.broadcast()
 
                 if res:
@@ -403,8 +403,8 @@ class R:
                             if isinstance(k, int) and k == id(self):
                                 op = item.pop()
                                 break
-                        for char in prev_str[op + 1:res.ed + 1]:
-                            and_res_l = self.and_r.broadcast(char, prev_str)
+                        for char in prev_l[op + 1:res.ed + 1]:
+                            and_res_l = self.and_r.broadcast(char, prev_l)
                         self.and_r.broadcast()
 
                         res.as_fail()
@@ -413,7 +413,7 @@ class R:
                                 res.as_success()
                                 res.capture_t += and_res.capture_t
             for or_r in self.or_r_l:
-                or_r_res_l = or_r.broadcast(char, prev_str)
+                or_r_res_l = or_r.broadcast(char, prev_l)
                 self_res_l.extend(or_r_res_l)
 
         for res in filter(bool, self_res_l):
@@ -482,12 +482,12 @@ class R:
     def match(self, source: Iterable):
         self.is_top = True
         res_l = []
-        prev_str = []
+        prev_l = []
         for i, char in enumerate(chain([EOF], source, [EOF])):
             i -= 1
-            prev_str.append(char)
+            prev_l.append(char)
             self.active(Res(i, i, capture_t=((id(self), i),)))
-            res_l.extend(agl_update(self.broadcast(char, prev_str, i)))
+            res_l.extend(agl_update(self.broadcast(char, prev_l, i)))
         res_l = agl_filter(res_l)
         self.broadcast()
         self.is_top = False
