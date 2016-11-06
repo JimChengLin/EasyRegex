d6a82c0eb07372e5f59a5d423b9423050ce6c14a
Jim <jimzuolin@gmail.com>
2016-11-06 20:33:52 +0800

    细粒度二级缓存

--- R/cache.py
+++ R/cache.py
@@ -1,30 +1,33 @@
-from collections import OrderedDict
-
 if False:
     # 仅用于类型检查
     from .Result import Result
     from .R import R
 
 # {..., k: (share_l, share_iter)}
-cache = OrderedDict()
+pri_cache = {}
+# {..., k: (pri_k, offset)}
+sec_cache = {}
 
 
 def cache_clear():
-    cache.clear()
+    pri_cache.clear()
+    sec_cache.clear()
 
 
 def cache_deco(imatch):
+    '''
+    一级缓存
+    缓存 R 中 imatch 的修饰器
+    '''
+
     def memo_imatch(self: 'R', resource: str, prev_result: 'Result'):
-        def tpl(result: 'Result'):
-            return id(self), id(resource), str(result)
+        def tpl(res: 'Result'):
+            return id(self), id(resource), str(res)
 
         k = tpl(prev_result)
-        share_l, share_iter = cache.setdefault(k, ([], imatch(self, resource, prev_result)))
-        cache.move_to_end(k)
-        if len(cache) > 128:
-            cache.popitem(last=False)
-
+        share_l, share_iter = pri_cache.setdefault(k, ([], imatch(self, resource, prev_result)))
         yield from share_l
+
         while True:
             try:
                 echo = next(share_iter)
@@ -34,3 +37,13 @@ def cache_deco(imatch):
             yield echo
 
     return memo_imatch
+
+
+# 二级缓存
+def add_sec_cache(r: 'R', resource: str, prev_result: 'Result', curr_result: 'Result'):
+    def tpl(res: 'Result'):
+        return id(r), id(resource), str(res)
+
+    pri_k = tpl(prev_result)
+    sec_k = tpl(curr_result)
+    sec_cache[sec_k] = (pri_k, len(pri_cache[pri_k][0]))
